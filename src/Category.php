668d8ebb7c29bf1e71f2e87cd58d16ce64b4d710
Ata <quetzalskeith@gmail.com>
2017-07-15 22:24:35 +0700

    Checking checkbox

--- src/Category.php
+++ src/Category.php
@@ -32,7 +32,7 @@ class Category extends Model
         return $el;
     }
 
-    public static function nestedCheckbox($parentId = null)
+    public static function nestedCheckbox($parentId = null, $lists = null)
     {
         $categories = Category::select('id', 'name')
             ->where('parent_id', $parentId)
@@ -42,12 +42,19 @@ class Category extends Model
         $el = '';
 
         foreach ($categories as $category) {
+            $filtered = collect($lists)->filter(function ($value, $key) use ($category) {
+                return $value->id == $category->id;
+            })->count();
+
+            $checked = ($filtered == 0 )? '' : 'checked';
+
             $el .= '<li>';
-            $el .= '<label><input class="uk-checkbox" type="checkbox" name="categories[]" value="'.$category->id.'"> '.$category->name.'</label>';
+
+            $el .= '<label><input class="uk-checkbox" type="checkbox" name="categories[]" value="'.$category->id.'" '.$checked.'> '.$category->name.'</label>';
 
             if ($category->has('children')){
                 $el .= '<ul>';
-                    $el .= $category->nestedCheckbox($category->id);
+                    $el .= $category->nestedCheckbox($category->id, $lists);
                 $el .= '</ul>';
             }
 
@@ -55,6 +62,15 @@ class Category extends Model
         }
 
         return $el;
+    }
+
+    public function isChecked($lists = null)
+    {
+        if (is_null($lists)) return false;
+
+        if (in_array($this->id, $lists)) return true;
+
+        return false;
     }
 
     public function children()
